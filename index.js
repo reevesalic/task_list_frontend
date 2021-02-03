603805b0fe2487e530e2d7c823852f31febf007b
reevesalic <reevesalic@gmail.com>
2021-02-03 10:29:23 -0500

    added fetch - backend

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const endPoint = 'http://localhost:3000/api/v1/tasks'
 document.addEventListener('DOMContentLoaded', () => {
      // fetch and load task list
-     console.log("DOM is Loaded");
-     getTasks()
+     fetch(endPoint)
+     .then(response => response.json)
+     .then(tasks => {
+          console.log(tasks)
+     })
+     // getTasks()
+})
