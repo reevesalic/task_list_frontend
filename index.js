64117d0bf6512d498a380dad156879b5664f518a
reevesalic <reevesalic@gmail.com>
2021-02-04 14:18:27 -0500

    added queryselector to index.js file

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const endPoint = 'http://localhost:3000/api/v1/tasks'
 document.addEventListener('DOMContentLoaded', () => {
+     console.log("DOM is Loaded")
+     getTasks
      // fetch and load task list
+     function getTasks(){
      fetch(endPoint)
      .then(response => response.json)
      .then(tasks => {
-          console.log(tasks)
+          tasks.data.forEach(tasks => {
+               let newTask = new tasks(task, task.attributes.task)
+               document.querySelector('tasks-container').innerHTML += newSyllabus.renderTaskCard()
+               
+          })
      })
      // getTasks()
+}
 })
