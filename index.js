9741c62f5b2a42e65e7c0e8fc5661d450b5e83a2
reevesalic <reevesalic@gmail.com>
2021-02-05 10:32:38 -0500

    changed text boxes in html. updated index.js with post request.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const endPoint = 'http://localhost:3000/api/v1/tasks'
 document.addEventListener('DOMContentLoaded', () => {
-     
+     console.log("DOM is Loaded");
      getTasks()
 
-     const createTasksForm = document.querySelector("create-task-form")
-     createTasksForm.addEventListener("submit", (e) => createFormHandler(e))
-     
+     const createTaskForm = document.querySelector("create-task-form")
+    
+     createTaskForm.addEventListener("submit", (e) => createFormHandler(e))
+})
      // fetch and load task list
      function getTasks(){
      fetch(endPoint)
@@ -16,7 +17,29 @@ document.addEventListener('DOMContentLoaded', () => {
                document.querySelector('tasks-container').innerHTML += newSyllabus.renderTaskCard()
                
           })
+    
      })
-     // getTasks()
 }
-})
+
+function createFormHandler(e) {
+     e.preventDefault()
+     const taskInput = document.querySelector('#input-task').value
+     const descriptionInput = document.querySelector('#input-description').value
+     const categoryId = parseInt(document.querySelector('#categories').value)
+     postFetch(taskInput, descriptionInput, categoryId)
+}
+function postFetch(task, description, category_id) {
+     const bodyData = {task, description, category_id}
+     fetch(endPoint, {
+          method: "POST",
+          headers: {"Content-Type": "application/json"}
+          body: JSON.stringify(bodyData)
+     })
+     .then(response => response.json())
+     .then(task => {
+          console.log(task);
+          const taskData = task.data
+          let newTask = newSyllabus(syllabusData, syllabusData.attributes)
+          document.querySelector('#task-container').innerHTML += newTask.renderTaskCard()
+     })
+}
