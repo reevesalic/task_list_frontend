c9fa1b7c9a34bc668040eb0767359c7291395c40
reevesalic <reevesalic@gmail.com>
2021-02-09 11:50:41 -0500

    fixed category and added edit form functionality

--- task.js
+++ task.js
@@ -14,8 +14,37 @@ renderTaskCard() {
      
             <h5>${this.task}</h5>
             <p>${this.description}</p>
-            <p">Category: ${this.category.name}</p>
+            <p">Category: ${this.category.category}</p>
             <button data-id=${this.id}>Edit</button>`
+            
+}
+
+renderUpdateForm() {
+  return `
+  <form data-id=${this.id} >
+    <h3>Edit a Task!</h3>
+
+    <label>Task</label>
+    <input id='input-task' type="text" name="task" value="${this.task}" class="input-text">
+    <br><br>
+
+    <label>Description</label>
+    <textarea id='input-description' name="description" rows="8" cols="80" value="">${this.description}</textarea>
+    <br><br>
+
+  
+    <label>Category</label>
+    <select id="categories" name="categories" value="${this.category.category}">
+      <option value="1">Financial</option>
+      <option value="2">Health</option>
+      <option value="3">Misc</option>
+    </select>
+    <br><br>
+
+    <input id='edit-button' type="submit" name="submit" value="Edit Task" class="submit">
+  </form>
+`;
 }
 }
+
 Task.all = [];
