f7ee378204942683cf17d74d366573961da0e2a4
reevesalic <reevesalic@gmail.com>
2021-02-16 09:42:42 -0500

    worked on delete functionality

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
      getTasks()
 
      const createTaskForm = document.querySelector("#create-task-form")
-    
+
      createTaskForm.addEventListener("submit", (e) => createFormHandler(e))
-     
+
 })
-     // fetch and load task list
-     function getTasks() {
+// fetch and load task list
+function getTasks() {
      fetch(endPoint)
-     .then(response => response.json())
-     .then(tasks => {
-          tasks.data.forEach(task => {
-               let newTask = new Task(task, task.attributes)
-               document.querySelector('#tasks-container').innerHTML += newTask.renderTaskCard()
-               //delete button and display message.
-               const matches = document.querySelectorAll('.delete'); 
-          matches.forEach((item)=>{
-               item.onclick=function(){
-                    alert("deleted!")
-               }
+          .then(response => response.json())
+          .then(tasks => {
+               tasks.data.forEach(task => {
+                    let newTask = new Task(task, task.attributes)
+                    document.querySelector('#tasks-container').innerHTML += newTask.renderTaskCard()
+                    //delete button and display message.
+                    const matches = document.querySelector('#tasks-container').querySelectorAll('.delete');
+                    matches.forEach((item, index) => {
+                         item.onclick = ((e)=> {
+                              e.preventDefault(); 
+                              e.stopPropagation();
+                              const filteredItems = tasks.data.filter(x => x.id !== index);
+                              tasks.data = filteredItems;
+                         })
+                    })
+               })
           })
-        })
-     })
 }
 
 function createFormHandler(e) {
@@ -36,41 +39,41 @@ function createFormHandler(e) {
 }
 function postFetch(task, description, category_id) {
      console.log(task, description, category_id)
-     const bodyData = {task: {task, description, category_id}}
+     const bodyData = { task: { task, description, category_id } }
      fetch(endPoint, {
           method: "POST",
-          headers: {"Content-Type": "application/json"},
+          headers: { "Content-Type": "application/json" },
           body: JSON.stringify(bodyData)
      })
-     .then(response => response.json())
-     .then(task => {
-          
-          const taskData = task.data
-          let newTask = new Task(taskData, taskData.attributes)
-          const container =
-          document.querySelector('#tasks-container').innerHTML += newTask.renderTaskCard()
-          const matches = document.querySelectorAll('.delete'); 
-          matches.forEach((item)=>{
-               item.onclick=function(){
-                    alert("deleted!")
-               }
+          .then(response => response.json())
+          .then(task => {
+
+               const taskData = task.data
+               let newTask = new Task(taskData, taskData.attributes)
+               const container =
+                    document.querySelector('#tasks-container').innerHTML += newTask.renderTaskCard()
+               // const matches = document.querySelectorAll('.delete'); 
+               // matches.forEach((item)=>{
+               //      item.onclick=function(){
+               //           alert("deleted!")
+               //      }
+               // })
           })
-     })
 
-          //checkbox
-     completeTasksHolder.addEventListener("onclick", function(complete) {
-          if (complete.target && complete.target.matches("input[type=checkbox]")) {
-             let checkbox = complete.target;
-             let task = checkbox.parentNode;
-              checkbox.setAttribute("complete");
-              completeTasksHolder.appendChild(task);  
-          }
-        });
+     //checkbox
+     // completeTasksHolder.addEventListener("onclick", function (complete) {
+     //      if (complete.target && complete.target.matches("input[type=checkbox]")) {
+     //           let checkbox = complete.target;
+     //           let task = checkbox.parentNode;
+     //           checkbox.setAttribute("complete");
+     //           completeTasksHolder.appendChild(task);
+     //      }
+     // });
 
-          //delete task
-        function removeTask(task) {
-          let taskToRemove = document.getElementById(task);
-          taskToRemove.parentNode.removeChild(taskToRemove);
-  }
- } 
+     //delete task
+     function removeTask(task) {
+          // let taskToRemove = document.getElementById(task);
+          task.parentNode.removeChild(task);
+     }
+}
 
