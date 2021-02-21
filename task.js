c26f77b245572653f73c2692816c70850c14a367
reevesalic <reevesalic@gmail.com>
2021-02-21 13:07:57 -0500

    applied styling

--- task.js
+++ task.js
@@ -10,19 +10,18 @@ class Task {
     console.log(this)
   }
 
-  
 renderTaskCard() {
      return `
      
-     <div>
+     <div class="tasklist">
      
-     <h5> <input type="checkbox" class="complete"><label for="complete" ${this.complete}> </label> Task: ${this.task}</h5>
+     <h4> <input type="checkbox" class="complete"><label for="complete" ${this.complete}> </label> Task: ${this.task}</h4>
             <p>Description: ${this.description}</p>
             <p">Category: ${this.category.category}</p>
             <input id="delete" type="button" name="delete" value="Delete Task" class="delete" data-id = ${this.id}>
             <br>
 </div> 
-<br>`
+`
             
 }
 }
