b0ae649e413a4294a89b04dc2e831e7064c3b3de
reevesalic <reevesalic@gmail.com>
2021-02-26 13:51:25 -0500

    added delete functionality and checkbox functionality

--- index.js
+++ index.js
@@ -9,49 +9,53 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 // fetch and load task list
 function getTasks() {
+    let ttaskList = [];
     fetch(endPoint)
         .then((response) => response.json())
         .then((tasks) => {
             tasks.data.forEach((task) => {
                 let newTask = new Task(task, task.attributes);
                 document.querySelector("#tasks-container").innerHTML += newTask.renderTaskCard();
+                ttaskList.push(newTask);                                                                      
 
-                //delete button and display message.
-                const matches = document.querySelector("#tasks-container").querySelectorAll(".delete");
-                matches.forEach((button, index) => {
-                    button.onclick = (e) => {
-                        const filteredItems = tasks.data.filter((x) => x.id !== index);
-                        tasks.data = filteredItems;
-
-                        fetch(`http://localhost:3000/api/v1/tasks/${button.dataset.id}`, {
-                            method: "DELETE",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify(task),
-                        });
-                        window.location.reload();
-                    };
+                           
+            });
+        }).then(() => {
+            let tContain = document.querySelector("#tasks-container");
+            completeChecks = tContain.querySelectorAll(".complete");
+            for (const c of completeChecks) {
+                const specificTask = ttaskList.filter((x) => x.id === c.parentNode.parentNode.id.toString());
+                c.addEventListener('change', (e) => {
+                    e.preventDefault();
+                    const ID = e.target.parentNode.parentNode.id;
+                    const complete = e.target.checked;
+                
+                    setCheckComplete(specificTask, ID, complete);
                 });
-                //checkbox
-               //  const matches = document.querySelector("#tasks-container").querySelectorAll(".complete");
-               //  matches.forEach((checkbox, index) => {
-               //      checkbox.onclick = (e) => {
-               //          const filteredItems = tasks.data.filter((x) => x.id !== index);
-               //          tasks.data = filteredItems;
+            }
 
-               //          fetch(`http://localhost:3000/api/v1/tasks/${checkbox.dataset.id}`, {
-               //              method: "POST",
-               //              headers: {
-               //                  "Content-Type": "application/json",
-               //              },
-               //              body: JSON.stringify(task),
-               //          });
-               //          window.location.reload();
-               //      };
-               //  });
+            //delete button and display message.
+            const matches = document.querySelector("#tasks-container").querySelectorAll(".delete");
+            matches.forEach((button, index) => {
+                button.onclick = (e) => {
+                    const filteredItems = ttaskList.filter((x) => x.id !== index);
+                    
+                    fetch(`http://localhost:3000/api/v1/tasks/${button.dataset.id}`, {
+                        method: "DELETE",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(filteredItems),
+                        
+                    });
+                    let delTask = ttaskList.filter((x) => x.id === e.target.parentNode.parentNode.id);
+
+                    document.getElementById(delTask[0].id).remove();
+
+                };
             });
         });
+        
 }
 
 function createFormHandler(e) {
@@ -61,6 +65,18 @@ function createFormHandler(e) {
     const categoryId = parseInt(document.querySelector("#categories").value);
     postFetch(taskInput, descriptionInput, categoryId);
 }
+
+function setCheckComplete(task, id, complete) {
+    console.log(task, id, complete);
+    task.complete = complete;
+    const bodyData = { task: { task, complete } };
+    fetch(endPoint + "/" + id, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(bodyData),
+    }).then((response) => response.json())
+}
+
 function postFetch(task, description, category_id) {
     console.log(task, description, category_id);
     const bodyData = { task: { task, description, category_id } };
@@ -74,18 +90,7 @@ function postFetch(task, description, category_id) {
             const taskData = task.data;
             let newTask = new Task(taskData, taskData.attributes);
             const container = (document.querySelector("#tasks-container").innerHTML += newTask.renderTaskCard());
-            const matches = document.querySelectorAll(".delete");
-            matches.forEach((item) => {
-                item.onclick = function () {
-                    alert("deleted!");
-                };
-            });
+            window.location.reload();
         });
 
-   
-
-    //delete task
-    function removeTask(task) {
-        task.parentNode.removeChild(task);
-    }
 }
